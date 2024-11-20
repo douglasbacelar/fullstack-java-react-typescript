1821894437eaaf6bbfe6503393f016bdb7299e91
Douglas Bacelar <bacelardouglas@outlook.com>
2024-11-20 17:46:42 -0300

    ajustando cors

--- cardapio-backend/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio-backend/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "*")
 @RequestMapping("food")
 public class FoodController {
 
